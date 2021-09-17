89adca69c359dac46c97314387b8bbe657a8a704
Michael Sanchez <michael.joshua.sanchez@gmail.com>
2021-09-17 12:51:49 -0700

    SQL table exercises 7/17/21

---
+++ tables_exercises.sql
@@ -0,0 +1,11 @@
+USE employees;
+SHOW TABLES;
+DESCRIBE employees;
+DESCRIBE departments;
+SHOW CREATE TABLE dept_manager;
+
+-- employees table contains int, date, varchar, and enum data types
+-- salaries should contain a numeric data type
+-- employees should contain strings
+-- dept_emp_latest_date should contain date type
+-- employee number can be linked to department number in table dept_emp
